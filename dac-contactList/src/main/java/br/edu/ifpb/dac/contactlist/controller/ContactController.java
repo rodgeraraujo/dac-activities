796d2fc634ef935c2cef4f0bbbfcb1e5a89d1d4b
fernandasj <fernanda.silva_sj@hotmailcom>
2019-03-23 18:58:34 -0300

    creating CRUD JPA

--- dac-contactList/src/main/java/br/edu/ifpb/dac/contactlist/controller/ContactController.java
+++ dac-contactList/src/main/java/br/edu/ifpb/dac/contactlist/controller/ContactController.java
@@ -1,5 +1,53 @@
 package br.edu.ifpb.dac.contactlist.controller;
 
+import br.edu.ifpb.dac.contactlist.model.Contact;
+import infra.ManagerContact;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class ContactController {
     
+    private EntityManager em = Persistence
+                .createEntityManagerFactory("contactList")
+                .createEntityManager();
+    
+    public boolean saveContact(Contact contact) {
+        ManagerContact mc = new ManagerContact(em);
+        mc.save(contact);
+        return true;
+    }
+    
+    public Contact searchContactByName(String name) {
+        ManagerContact mc = new ManagerContact(em);
+        Contact contact = mc.search(name);
+        return contact;
+    }
+    
+    public Contact updateContact(int id,Contact c) {
+        ManagerContact mc = new ManagerContact(em);
+        Contact contact = mc.update(id, c);
+        return contact;
+    }
+    
+    public Contact remove(long id){
+        ManagerContact mc = new ManagerContact(em);
+        Contact contact = mc.remove(id);
+        return contact;
+    }
+    
+    public List<Contact> listAllContacts() {
+        ManagerContact mc = new ManagerContact(em);
+        return mc.listAll();
+    }
+    
+    public List<Contact> listContactsOrderByName(){
+        ManagerContact mc = new ManagerContact(em);
+        return mc.listOrderByName();
+    }
+    
+    public List<Contact> listContactsGroup(String letter){
+        ManagerContact mc = new ManagerContact(em);
+        return mc.listGroup(letter);
+    }
 }
--- dac-contactList/src/main/java/br/edu/ifpb/dac/contactlist/model/Contact.java
+++ dac-contactList/src/main/java/br/edu/ifpb/dac/contactlist/model/Contact.java
@@ -1,8 +1,16 @@
 package br.edu.ifpb.dac.contactlist.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-public class Contact {
+@Entity
+public class Contact implements Serializable{
+    @Id
+    @GeneratedValue
+    private long id;
     private String name; 
     private String email;
     private String phone;
@@ -10,13 +18,25 @@ public class Contact {
 
     public Contact() {
     }
-
     
-    public Contact(String name, String email, String phone, LocalDate birthday) {
+    public Contact(long id, String name, String email, String phone, LocalDate birthday) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.birthday = birthday;
+    }
+
+    public static Contact empty() {
+        return new Contact(-1,"","", "", null);
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -50,7 +70,4 @@ public class Contact {
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
-    
-    
-
 }
