67ca6c219fae38d906c7122a1b31ef8674efbca3
fernandasj <fernanda.silva_sj@hotmailcom>
2019-03-23 19:01:46 -0300

    fix Docker

--- dac-contactList/run.sh
+++ dac-contactList/run.sh
@@ -3,12 +3,3 @@ docker run -p 5433:5432 -d --name database payara/database
 mvn clean package
 docker build -t payara/app .
 docker run -p 4848:4848 -p 8081:8080 -d --name app --link database:host-banco payara/app
-
-
-
-
-
-
-
-# docker run -p 8082:8080 --name app --link database:host-banco -d payara/app 
-# docker run -p 8081:8080 -d --name app payara/apps
